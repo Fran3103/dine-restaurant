cfe89b63feb430b6f894c9ea7fd78b6bb385a208
Fran3103 <fn.aguirre@hotmail.com>
2024-02-28 12:21:10 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-center text-red-500 mr-12">hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors:{
+            'Beaver': '#9e7f66',
+            'Cod Gray': '#111111',
+
+            'Mirage': '#17192b',
+            'Ebony-Clay': '#242b37',
+            'Shuttle Gray': '#5c6779 '
+
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
