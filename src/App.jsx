5a42e3267a59c00bc12733892bb5cdb71fa13497
Fran3103 <fn.aguirre@hotmail.com>
2024-02-28 23:06:41 -0300

    avances varios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./assets/Components/Home"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-center text-red-500 mr-12">hola mundo</h1>
+      <Home/>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ export default {
     theme: {
         colors:{
             'Beaver': '#9e7f66',
-            'Cod Gray': '#111111',
+            'Cod-Gray': '#111111',
 
             'Mirage': '#17192b',
             'Ebony-Clay': '#242b37',
-            'Shuttle Gray': '#5c6779 '
+            'Shuttle Gray': '#5c6779 ',
+            'white': '#ffffff'
 
+        },
+       
+        backgroundImage:{
+            'mobile': "url('/src/assets/images/homepage/hero-bg-mobile.webp')",
+            'tablet': "url('/src/assets/images/homepage/hero-bg-tablet.webp')",
+            'desktop': "url('/src/assets/images/homepage/hero-bg-desktop.webp')",
         },
       extend: {},
     },
